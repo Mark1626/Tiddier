a29b7a7d7fdaa84bc01cf0f52ca6e65c57458fce
Mark1626 <nimalan.m@protonmail.com>
2021-06-30 21:10:13 +0530

    Update CSS override

--- tiddier.js
+++ tiddier.js
@@ -5,10 +5,10 @@ prettify.onclick = function () {
     chrome.tabs.insertCSS(tabs[0].id, {
       code: `
       body {
-        width: 80vw;
-        margin: auto;
-        line-height: 1.7;
-        font-size: 1.19em;
+        width: 70vw;
+        margin: auto !important;
+        line-height: 1.7 !important;
+        font-size: 1.19rem !important;
         font-family: "Open Sans", sans-serif;
       }
       `
